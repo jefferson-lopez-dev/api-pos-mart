a407b4d55335b20d4bd66485b38ef324f7a079b1
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-10 13:46:53 -0500

    create routes

---
+++ src/apps/folder/infrastructure/routes/folder.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { MongoRepository as Repository } from "../repository/mongo";
+import { FoldersUseCase as UseCase } from "../../app/useCase";
+import { FolderController as Controllers } from "../controllers";
+
+export const FolderRoute = Router();
+
+const MongoFolderRepository = new Repository();
+const folderUseCase = new UseCase(MongoFolderRepository);
+const {
+  SearchAFolder,
+  SearchForAnInventoryFolder,
+  SearchInventoryFolders,
+  searchAllFolders,
+  createFolder,
+  updateFolder,
+  deleteFolder,
+} = new Controllers(folderUseCase);
+
+FolderRoute.post("/search/allFolders", searchAllFolders);
+FolderRoute.post("/search/aFolder", SearchAFolder);
+FolderRoute.post("/search/forAnInventoryFolder", SearchForAnInventoryFolder);
+FolderRoute.post("/search/inventoryFolders", SearchInventoryFolders);
+FolderRoute.post("/", createFolder);
+FolderRoute.put("/", updateFolder);
+FolderRoute.delete("/:folder_uuid", deleteFolder);
