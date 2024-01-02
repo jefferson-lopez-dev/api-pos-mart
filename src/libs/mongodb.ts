91ba7710307023c7965e273fc11fb0f59cd123d7
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-01-01 19:59:53 -0500

    connect db

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -3,14 +3,14 @@ import { config } from "dotenv";
 
 config();
 
-// const { URL_MONGO_DB_ATLAS } = process.env;
+const { URL_MONGO_DB_ATLAS } = process.env;
 
-// const URL_DATABASE = URL_MONGO_DB_ATLAS;
-const URL_DATABASE_LOCAL = "mongodb://localhost:27017/mart-mvp";
+const URL_DATABASE = URL_MONGO_DB_ATLAS || "mongodb://localhost:27017/mart-mvp";
+// const URL_DATABASE_LOCAL = "mongodb://localhost:27017/mart-mvp";
 
 export const ConnectionDB = async () => {
   try {
-    await connect(URL_DATABASE_LOCAL);
+    await connect(URL_DATABASE);
     console.log("● DB is connected");
   } catch (error) {
     console.log("● Disconnected to db", error);
